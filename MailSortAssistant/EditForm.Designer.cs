627e5bc388992724222b8a07268174639e042f16
BOWDEN <kb0783332@otc.edu>
2017-03-01 19:25:27 -0600

    Update

---
+++ MailSortAssistant/EditForm.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace MailSortAssistant
+{
+    partial class EditForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtBxLastName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtBxFirstName = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtBxDept = new System.Windows.Forms.TextBox();
+            this.txtBxNote = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSubmit.Location = new System.Drawing.Point(12, 492);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(148, 49);
+            this.btnSubmit.TabIndex = 0;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancel.Location = new System.Drawing.Point(208, 492);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(150, 49);
+            this.btnCancel.TabIndex = 1;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(7, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(121, 25);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Last Name:";
+            // 
+            // txtBxLastName
+            // 
+            this.txtBxLastName.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBxLastName.Location = new System.Drawing.Point(12, 38);
+            this.txtBxLastName.Name = "txtBxLastName";
+            this.txtBxLastName.Size = new System.Drawing.Size(226, 31);
+            this.txtBxLastName.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(7, 113);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(122, 25);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "First Name:";
+            // 
+            // txtBxFirstName
+            // 
+            this.txtBxFirstName.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBxFirstName.Location = new System.Drawing.Point(12, 141);
+            this.txtBxFirstName.Name = "txtBxFirstName";
+            this.txtBxFirstName.Size = new System.Drawing.Size(226, 31);
+            this.txtBxFirstName.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(7, 215);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 25);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Dept:";
+            // 
+            // txtBxDept
+            // 
+            this.txtBxDept.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBxDept.Location = new System.Drawing.Point(12, 243);
+            this.txtBxDept.Name = "txtBxDept";
+            this.txtBxDept.Size = new System.Drawing.Size(226, 31);
+            this.txtBxDept.TabIndex = 7;
+            // 
+            // txtBxNote
+            // 
+            this.txtBxNote.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBxNote.Location = new System.Drawing.Point(12, 338);
+            this.txtBxNote.Multiline = true;
+            this.txtBxNote.Name = "txtBxNote";
+            this.txtBxNote.Size = new System.Drawing.Size(346, 129);
+            this.txtBxNote.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(7, 310);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(63, 25);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Note:";
+            // 
+            // EditForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.ClientSize = new System.Drawing.Size(373, 552);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtBxNote);
+            this.Controls.Add(this.txtBxDept);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtBxFirstName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtBxLastName);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnSubmit);
+            this.Name = "EditForm";
+            this.Text = "Edit Faculty";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtBxLastName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtBxFirstName;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtBxDept;
+        private System.Windows.Forms.TextBox txtBxNote;
+        private System.Windows.Forms.Label label4;
+    }
+}
---
+++ MailSortAssistant/EditForm.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MailSortAssistant
+{
+    /// <summary>
+    /// Form: This form will allow the user to edit or add an faculty member to the excel worksheet.
+    /// </summary>
+    public partial class EditForm : Form
+    {
+        // Class Level Variables.
+        public EditForm()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// When the use presses this button the data in the text boxes will be commited to memory.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            //TODO
+        }
+
+        /// <summary>
+        /// When the user presses this button the data in the text boxes will not be commited to memoery and the form will exit back to form 1.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            //TODO
+        }
+    }// End of Class.
+}// End of Solution.
